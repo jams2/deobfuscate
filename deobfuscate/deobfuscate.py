58e85f3e5135ea6487c98a51706bb5efe6bb2f2b
jams2 <joshamunn@gmail.com>
2018-06-12 11:13:16 +0100

    update argparser/usage

--- deobfuscate/deobfuscate.py
+++ deobfuscate/deobfuscate.py
@@ -12,10 +12,10 @@ import re
 
 def generate_linebreaks(tokens, *args):
     """
-    tokens: list or str to split into tokens.
-    *args: specify delimiters, (defaults to ';' if none specified)
+    tokens (list | str)
+    *args (optional delimiters, defaults to ';')
     Wrapper for recur_split.
-    Returns a list of tokens split on ';', or specified delimiters.
+    Returns a list of tokens split on delim.
     """
     if not isinstance(tokens, str) and not isinstance(tokens, list):
         raise TypeError(f'Expected list or str, got {type(tokens)}')
@@ -29,6 +29,7 @@ def generate_linebreaks(tokens, *args):
 
 def recur_split(token, delim):
     """
+    token (list | str), delim (str)
     Splits token on delimiter, returns flat array of tokens.
     """
     if not token:
@@ -51,6 +52,7 @@ def recur_split(token, delim):
 
 def parse_hexchars(tokens):
     """
+    tokens (list)
     Find hexadecimal chars and substitute them with corresponding characters.
     """
     if not isinstance(tokens, list):
@@ -70,6 +72,7 @@ def parse_hexchars(tokens):
 
 def find_arrays(tokens):
     """
+    tokens (list)
     Read array declarations into a dict.
     """
     if not isinstance(tokens, list):
@@ -111,6 +114,7 @@ def parse_arrays(token):
 
 def nested_len(tokens):
     """
+    tokens (list | str)
     Count all items in list of lists (of lists...)
     """
     i = 0
@@ -129,6 +133,7 @@ def nested_len(tokens):
 
 def substitute_array_references(tokens, arrays):
     """
+    tokens (list), arrays (dict)
     Create list of tuples with (arrayname, pattern('arrayname[x]'),
     to substitute array references with actual values (use with care - doesn't account for
     arrays mutated after declaration).
@@ -147,7 +152,7 @@ def substitute_array_references(tokens, arrays):
             while index_match:
                 indices = get_indices(index_match[0])
                 new_token = (new_token.replace(index_match[0],
-                                               get_array_reference(arrays[pattern[0]], indices)))
+                                               get_array_item(arrays[pattern[0]], indices)))
                 index_match = re.findall(pattern[1], new_token)
         parsed_tokens.append(new_token)
     return parsed_tokens
@@ -155,6 +160,7 @@ def substitute_array_references(tokens, arrays):
 
 def get_indices(match):
     """
+    match (str): array reference
     Parse an (multidimensional) array reference, return a list of the indices.
     Example: 'array[1][2][3]' returns [1, 2, 3]
     """
@@ -171,9 +177,10 @@ def get_indices(match):
     return indices
 
 
-def get_array_reference(array, indices):
+def get_array_item(array, indices):
     """
-    Get nested list items.
+    array (list), indices (list->int)
+    Get item from (nested) list
     """
     for i in indices:
         array = array[i]
@@ -181,37 +188,52 @@ def get_array_reference(array, indices):
 
 
 def main():
+    """
+    If options not specified, execute all methods.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', help='.js file to parse', type=str)
     parser.add_argument('outfile', type=str, nargs='?', default=False,
-                        help='destination file, print to STDOUT if not specified')
+                        help='destination file, print to STDOUT if not specified.')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-l', '--insert-linebreaks', action='store_true',
-                       help='insert linebreaks after ";", or specify delimiters with -d')
-    group.add_argument('-d', '--delimiter', nargs='+', metavar='delim',
+    group.add_argument('-l', '--linebreaks', action='store_true',
+                       help='insert linebreaks after ";", or specify delimiters with -d.')
+    group.add_argument('-d', '--delimiter', nargs='+', metavar='<delim>',
                        help='space separated list of delimiters to break lines on. ' +
                        'characters such as ";" may need to be escaped.')
-    parser.add_argument('-x', '--parse-hex', action='store_true',
-                        help='parse hex encoded characters to ascii')
+    parser.add_argument('-x', '--hex', action='store_true',
+                        help='parse hex encoded characters to ascii.')
     parser.add_argument('-a', '--arrays', action='store_true',
                         help='substitute array references for declared values')
-    parser.add_argument('-r', '--range', nargs=2, type=int, metavar=('start', 'end'),
+    parser.add_argument('-r', '--range', nargs=2, type=int, metavar=('<start>', '<end>'),
                         help='range of lines to parse (non-inclusive). ' +
-                        'assumes infile contains linebreaks')
+                        'applied before other actions.') 
     args = parser.parse_args()
     with open(args.infile, 'r') as fd:
         tokens = fd.readlines()
-    if len(tokens) == 1:
-        tokens = generate_linebreaks(tokens, ';', ',')
-    tokens = parse_hexchars(tokens)
-    arrays = find_arrays(tokens)
-    #print(arrays)
-    #tokens = substitute_array_references(tokens, arrays)
+    if (not args.linebreaks and not args.delimiter and not args.hex and 
+        not args.arrays and not args.range):            
+        tokens = generate_linebreaks(tokens)
+        tokens = parse_hexchars(tokens)
+        tokens = substitute_array_references(tokens, find_arrays(tokens))
+    else:
+        if args.range:
+            tokens = tokens[args.range[0], args.range[1]]
+        if args.linebreaks:
+            tokens = generate_linebreaks(tokens)
+        elif args.delimiter:
+            tokens = generate_linebreaks(tokens, *args.delimiter)
+        if args.hex:
+            tokens = parse_hexchars(tokens)
+        if args.arrays:
+            tokens = substitute_array_references(tokens, find_arrays(tokens))
     if args.outfile:
         with open(args.outfile, 'w') as fd:
             fd.write('\n'.join(tokens))
+    else:
+        for token in tokens:
+            print(token)
 
 
 if __name__ == '__main__':
-
     main()
